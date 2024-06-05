3a35698c9a1482e040aea1dee4698d1a8940a224
dzaky insan <dzakyinsan@dzakys-MacBook-Air.local>
2024-06-05 15:14:58 +0700

    delete log

--- src/components/CompanyDesc.tsx
+++ src/components/CompanyDesc.tsx
@@ -40,22 +40,6 @@ const CompanyDescription = ({ handleButtonClick }: any) => {
     </div>
   );
   {
-    /* // return (
-  //   <section className="h-[80vh] flex items-center text-center mb-12 px-10 md:px-40 xl:px-60 card-bg">
-  //     <div className="flex flex-col gap-3 animate-fade-in
-  //     ">
-  //       <h1 className="font-bold text-5xl mb-4 ">About Our Company</h1>
-  //       <p className="text-[#888] font-thin">
-  //         Lorem ipsum dolor sit amet, <span className="text-white font-extralight">consectetur adipiscing elit.</span> Vivamus
-  //         lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-  //         malesuada. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint magnam assumenda neque corporis? Labore, nam asperiores numquam laborum, ut eaque accusantium similique voluptas unde nihil veniam aspernatur. 
-  //       </p>
-  //       <div className="flex justify-center items-center">
-  //         <Image src={DownArrow} alt='down-arrow' height={20} width={20} className="animate-bounce mt-10 cursor-pointer" onClick={handleButtonClick}/>
-  //       </div>
-  //     </div>
-  //   </section>
-  // ); */
   }
 };
 
